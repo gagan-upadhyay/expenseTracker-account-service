2fa42f4e9e01a410259616e4f8fb9ed029709569
Upadhyay <gaganupadhyay.karan@gmail.com>
2026-03-24 16:56:49 +0530

    Added frontend address on the cors

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ import { helmetConfig } from './config/helmet.config.js';
 const app = express();
 
 const corsOptions={
-    origin:['http://localhost:3000', 'https://expense-tracker-git-newbranch-gagans-projects-00cb1a77.vercel.app', 'http://192.168.0.126:3000',  'expense-tracker-self-rho-12.vercel.app', 'expense-tracker-gagans-projects-00cb1a77.vercel.app'],
+    origin:[
+        'http://localhost:3000',
+        'https://expense-tracker-git-newbranch-gagans-projects-00cb1a77.vercel.app',
+        'http://192.168.0.126:3000',
+        'https://expense-tracker-self-rho-12.vercel.app',
+        'https://expense-tracker-gagans-projects-00cb1a77.vercel.app'],
     credentials:true
 };
 // const corsOptions = {
